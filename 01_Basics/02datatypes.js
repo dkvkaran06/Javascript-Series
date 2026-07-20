7dc0c5c0457f8b28482a7ce5216fefc86e0c6135
Dev Karan <152410553+dkvkaran06@users.noreply.github.com>
2026-07-20 10:04:33 +0000

    Code

---
+++ 01_Basics/02datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are nodejs not browser 
+// There should be code readability high in our code , so that it looks good readable , understable
+let age =12
+console.log(3+3)
+console.log(typeof age)
+console.log(typeof "true")
+console.log(typeof false)
+console.log(typeof null) // it is object 
+console.log(typeof undefined) // it is undefined
