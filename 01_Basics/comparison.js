6946b79055cc7480f0484d5c17f81a8f28baf8fd
Dev Karan <152410553+dkvkaran06@users.noreply.github.com>
2026-07-21 09:40:52 +0000

    Comparison

---
+++ 01_Basics/comparison.js
@@ -0,0 +1,10 @@
+console.log(5>3)
+console.log(4<3)
+
+console.log("4">3) // here it converts that string into number 
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0) // here you can se both equity and greaterthanequalto works differently
+
+console.log("2"===2) /// triple equals to do not convert into number 
+console.log("2"==2)
