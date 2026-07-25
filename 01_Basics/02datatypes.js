34055d7130aad78037ca998a2f35382eb67440d8
Dev Karan <152410553+dkvkaran06@users.noreply.github.com>
2026-07-25 10:14:39 +0000

    changes

--- 01_Basics/02datatypes.js
+++ 01_Basics/02datatypes.js
@@ -11,17 +11,33 @@
 // console.log(typeof undefined) // it is undefined
 // let username;
 // console.log(username)
-const heroes=["dev","karan"] // array made
-let myobject ={name:"dev",// object having key value pairs
-    age: 22,
-}
-let name =function greet(){// funtion with stored inside variable here function take name of varibale, only can be called by varibable name as it is inside variable  
-    console.log("Hello")
-}
+// const heroes=["dev","karan"] // array made
+// let myobject ={name:"dev",// object having key value pairs
+//     age: 22,
+// }
+// let name =function greet(){// funtion with stored inside variable here function take name of varibale, only can be called by varibable name as it is inside variable  
+//     console.log("Hello")
+// }
 
-name()
-function abc() // abc as function name 
-{
-    console.log('how are u')
+// name()
+// function abc() // abc as function name 
+// {
+//     console.log('how are u')
+// }
+// abc() 
+
+//Stack memory for primitive data types
+let name ="dev"
+let y=name // here a copy of original value has been created 
+y="rdx"
+console.log(name) // original value is still no modification
+console.log(y) // here it gets update but orignal value be safe
+
+let userone={
+    name:"dev",
+    age:22
 }
-abc() 
+let usertwo=userone
+usertwo.age=23
+console.log(userone.age)
+console.log(usertwo.age)
